e4cb03816edb5e8ad13c806361cba6879f73e57f
sprili <slidedc@gmail.com>
2023-07-08 15:07:13 +0200

    style update

--- src/sites/virginmedia_page.py
+++ src/sites/virginmedia_page.py
@@ -29,66 +29,10 @@ class VirginMediaPage(BasePage):
     _is_credentials_is_correct_locator = "//*[contains(text(), ' Your email or password was incorrect')]"
     _captcha_iframe = "//iframe[starts-with(@src, 'https://www.google.com/recaptcha/api2/anchor')]"
 
-    @property
-    def _accept_cookies_button(self) -> WebElement:
-        return self._get_element(self._accept_cookies_locator)
-
-    # Web elements
-    @property
-    def login_input(self) -> WebElement:
-        return self._get_element(self._login_input_locator)
-
-    @property
-    def password_input(self) -> WebElement:
-        return self._get_element(self._password_input_locator)
-
-    @property
-    def login_form_button(self) -> WebElement:
-        return self._get_element(self._login_form_locator)
-
-    @login_input.setter
-    def login_input(self, login: str) -> NoReturn:
-        self.login_input.clear()
-        self.login_input.send_keys(login)
-
-    @password_input.setter
-    def password_input(self, password: str) -> NoReturn:
-        self.password_input.clear()
-        self.password_input.send_keys(password)
-
-    @property
-    def _recaptcha_response_element(self) -> WebElement:
-        return self._get_element("//*[@id='g-recaptcha-response']")
-
-    @property
-    def login_continue_button(self) -> WebElement:
-        return self._get_element(self._login_continue_locator)
-
-    @property
-    def latest_bill_button(self) -> WebElement:
-        return self._get_element(self._latest_bill_button_locator)
-
-    @property
-    def _capthca_site_key(self) -> str:
-        site_key_element = self._get_element(self._captcha_iframe)
-        return parse_qs(urlparse(site_key_element.get_attribute("src")).query)['k'][0]
-
-    @property
-    def close_bill_explainer_button(self) -> WebElement:
-        return self._get_element(self._close_bill_explainer_locator)
-
-    @property
-    def donwload_pdf_button(self) -> WebElement:
-        return self._get_element(self._download_pdf_button_locator)
-
-    @property
-    def is_credentials_is_correct_element(self) -> WebElement:
-        return self._get_element(self._is_credentials_is_correct_locator)
-
     # check is credentials are correct
     def _is_credentials_is_correct(self) -> bool:
         try:
-            self.is_credentials_is_correct_element
+            self._get_element(self._is_credentials_is_correct_locator)
             return False
         except Exception:
             return True
@@ -97,23 +41,22 @@ class VirginMediaPage(BasePage):
     def _accept_cookies_policy(self):
         try:
             logging.info(f"Trying to accept cookies")
-            if self._accept_cookies_locator != '':
-                self._accept_cookies_button.click()
-                time.sleep(1)
+            self._get_element(self._accept_cookies_locator).click()
             logging.info(f"Cookies policy accepted")
         except Exception:
             logging.info(f"No cookies policy")
 
     def _close_bill_explainer(self):
         try:
-            self.close_bill_explainer_button.click()
+            self._get_element(self._close_bill_explainer_locator).click()
         except Exception:
             pass
 
     # solving captcha method
     def _solve_captcha(self):
-        logging.info(f"Solving captcha: {str(self._capthca_site_key)}")
-        code = self._captcha_solver.recaptcha(sitekey=self._capthca_site_key, url=self._driver.current_url, invisible=1)['code']
+        captcha_site_key = parse_qs(urlparse(self._get_element(self._captcha_iframe).get_attribute("src")).query)['k'][0]
+        logging.info(f"Solving captcha: {str(captcha_site_key)}")
+        code = self._captcha_solver.recaptcha(sitekey=captcha_site_key, url=self._driver.current_url, invisible=1)['code']
         logging.info(f"Captcha was solved {code}")
         self._driver.execute_script(f'document.getElementById("g-recaptcha-response").innerHTML = "{code}"; submitForm();')
 
@@ -129,19 +72,27 @@ class VirginMediaPage(BasePage):
         time.sleep(2)
 
         # doing authorisation
-        self.login_form_button.click()
+        wait = WebDriverWait(self._driver, 10)  # wait up to 10 seconds
+        element = wait.until(EC.element_to_be_clickable((By.XPATH, self._login_form_locator)))
+        self._get_element(self._login_form_locator).click()
         logging.info(f"Input login: {self._login}")
-        self.login_input = self._login
+        self._get_element(self._login_input_locator).send_keys(self._login)
         logging.info(f"Login confirmed")
-        self.login_continue_button.click()
-        self.password_input = self._password
+
+        # opening next page with password and captcha
+        self._get_element(self._login_continue_locator).click()
+        self._get_element(self._password_input_locator).send_keys(self._password)
         logging.info(f"Password confirmed")
+
+        # solving captcha
         self._solve_captcha()
         logging.info(f"Captcha was solved")
+
+        # check is input credenatials correct
         assert self._is_credentials_is_correct(), "Wrong credentials"
 
         logging.info(f"Opening latest bill page")
-        self.latest_bill_button.click()
+        self._get_element(self._latest_bill_button_locator).click()
 
         # need to close bill explainer if it appears
         logging.info(f"Closing bill exaplainer")
@@ -156,6 +107,6 @@ class VirginMediaPage(BasePage):
         #click class="QSIWebResponsiveDialog-Layout1-SI_ezf2xkiB7FZrb38_close-btn"
 
         time.sleep(2)
-        self.donwload_pdf_button.click()
+        self._get_element(self._download_pdf_button_locator).click()
         time.sleep(2)
 
